4c2318b223ce4183580e42da7201aac6f4e3c5de
SomiJo <jodeirsomaye@gmail.com>
2023-10-15 10:53:44 +0500

    Updated the search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 export default function Weather() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [lastUpdated, setLastUpdated] = useState(null);
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+    const timestamp = response.data.dt * 1000;
+    const date = new Date(timestamp);
+    const lastUpdatedTime = date.toLocaleString();
+    setLastUpdated(lastUpdatedTime);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="Weather">
-      <form className="mb-3">
+      <form className="mb-3" onSubmit={handleSubmit}>
         <div className="row d-flex">
           <div className="col-9">
             <input
@@ -13,6 +43,7 @@ export default function Weather() {
               autocomplete="off"
               id="city-input"
               className="form-control shadow-sm"
+              onChange={updateCity}
             />
           </div>
           <div className="col-3">
@@ -25,45 +56,28 @@ export default function Weather() {
         </div>
       </form>
       <div className="overview">
-        <h1 id="city">New York</h1>
+        <h1 id="city">{city}</h1>
         <ul>
-          <li>Last updated: Tuesday 10:00</li>
-          <li id="description">Partly Cloudy</li>
+          <li>Last updated: {lastUpdated}</li>
+          <li id="description">{weather.description}</li>
         </ul>
       </div>
       <div className="row d-flex">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Cloudy icon"
-              className="weather-icon float-left"
-              id="weatherIcon"
-            />
+            <img src={weather.icon} alt={weather.description} />
             <div className="float-left">
-              <strong>19</strong>
-              <span class="units">
-                <a href="#" id="celsius-link">
-                  °C
-                </a>
-                |
-                <a href="#" id="fahrenheit-link">
-                  °F
-                </a>
-              </span>
+              <strong>Temperature: {Math.round(weather.temperature)}°C</strong>
             </div>
           </div>
         </div>
         <div class="col-6">
           <ul>
             <li>
-              Precipitation: <span id="precipitation">0</span>%
+              Humidity: <span id="humidity">{weather.humidity}</span>%
             </li>
             <li>
-              Humidity: <span id="humidity">77</span>%
-            </li>
-            <li>
-              Wind:<span id="speed">8</span> km/h
+              Wind:<span id="speed">{weather.wind}</span> km/h
             </li>
           </ul>
         </div>
